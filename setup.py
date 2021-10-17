aeee60de500387b8b46dc96acbd2477b99036b14
OOPA <zayintopology@gmail.com>
2021-10-17 23:56:49 +0200

    Archived (v0.3.1)

--- setup.py
+++ setup.py
@@ -1,45 +1,23 @@
 from setuptools import setup
+import os
+
+VERSION = "0.3.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
 
 setup(
-    name = 'astylo',
-    version = '0.3',
-    author = 'D. HU',
-    author_email = 'dangning.hu@cea.fr',
-    description = 'Python tool kit based on astropy, h5py, etc.',
-    license = 'BSD',
-    keywords = 'astronomy astrophysics',
-    url = 'https://github.com/kxxdhdn/astylo',
-    project_urls={
-        'IDL': 'https://github.com/kxxdhdn/astylo/tree/master/idlib',
-        'SwING': 'https://github.com/kxxdhdn/astylo/tree/master/swing_snippets',
-        'Tests': 'https://github.com/kxxdhdn/astylo/tree/master/tests',
-    },
-
-    python_requires='>=3.6',
-    install_requires = [
-        'numpy', 'scipy', 'matplotlib', 
-        'astropy', 'reproject', 'h5py', 'tqdm', 
-    ],
-    classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-    ],
-    
-    ## Plugins
-    entry_points={
-        # Installation test with command line
-        'console_scripts': [
-            'astyloading = astylo:iTest',
-        ],
-    },
-
-    ## Packages
-    packages = ['astylo'],
-
-    ## Package data
-    package_data = {
-        # include *.txt files in astylo/data
-        'astylo': ['dat/*.txt'],
-    },
+    name="astylo",
+    description="astylo is now laputan",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    version=VERSION,
+    install_requires=["laputan"],
+    classifiers=["Development Status :: 7 - Inactive"],
 )
